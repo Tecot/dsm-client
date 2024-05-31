c267da76dcd1bc0f139cdb770459dd6985b8b1b5
Tecot <8928590+tecot@user.noreply.gitee.com>
2024-05-31 18:01:26 +0800

    20240531

--- src/components/database/bioproject/SrpSearchConditions.vue
+++ src/components/database/bioproject/SrpSearchConditions.vue
@@ -93,41 +93,113 @@
           <div class="one-range-title">
             Depth value:
           </div>
-          <el-input 
+          <el-input-number 
             v-model="searchContent.depthValue" 
-            size="mini"
             placeholder="Please input depth value"
-          >
-          </el-input>
+            controls-position="right" 
+            size="mini"
+            style="width: 100%;">
+          </el-input-number>
         </div>
         <div class="between-range" v-if="searchContent.depthRangeCondition === 'Between([a,b])'">
           <div class="left">
             <div class="left-title">
               Low:
             </div>
-            <el-input 
+            <!-- <el-input 
               v-model="searchContent.depthRange.low" 
               size="mini"
               placeholder="Please input low value"
             >
-            </el-input>
+            </el-input> -->
+            <el-input-number 
+              v-model="searchContent.depthRange.low" 
+              placeholder="Please input low value"
+              controls-position="right" 
+              size="mini"
+              style="width: 100%;">
+            </el-input-number>
           </div>
           <div class="right">
             <div class="right-title">
               High:
             </div>
-            <el-input 
+            <!-- <el-input 
               v-model="searchContent.depthRange.high" 
               size="mini"
               placeholder="Please input high value"
             >
-            </el-input>
+            </el-input> -->
+            <el-input-number 
+              v-model="searchContent.depthRange.high" 
+              placeholder="Please input high value"
+              controls-position="right" 
+              size="mini"
+              style="width: 100%;">
+            </el-input-number>
           </div>
         </div>
       </div>
     </div>
 
+    <div class="search-condition-container">
+      <div class="title">
+        Longitude and latitude range
+      </div>
+      <div class="longitude-and-latitude-range-container">
+        
+          <div class="up">
+            <div class="left">
+              <div class="left-title">W:</div>
+              <el-input 
+                v-model="searchContent.llRange.w" 
+                size="mini"
+                placeholder="Please input W value"
+              >
+              </el-input>
+            </div>
+            <div class="right">
+              <div class="right-title">E:</div>
+              <el-input 
+                v-model="searchContent.llRange.E" 
+                size="mini"
+                placeholder="Please input E value"
+              >
+              </el-input>
+            </div>
+          </div>
 
+          <div class="down">
+            <div class="left">
+              <div class="left-title">S:</div>
+              <el-input
+                v-model="searchContent.llRange.S"  
+                size="mini"
+                placeholder="Please input S value"
+              >
+              </el-input>
+            </div>
+            <div class="right">
+              <div class="right-title">N:</div>
+              <el-input
+                v-model="searchContent.llRange.N"  
+                size="mini"
+                placeholder="Please input N value"
+              >
+              </el-input>
+            </div>
+          </div>
+      </div>
+    </div>
+
+    <div class="button-container">
+      <el-button size="mini" type="danger" @click="searchConfirm">
+        Confirm
+      </el-button>
+      <el-button size="mini" type="primary" @click="searchConditionsReset">
+        Reset
+      </el-button>
+    </div>
   </div>
 </template>
 
@@ -148,6 +220,12 @@ export default {
         depthRange: {
           low: '',
           high: ''
+        },
+        llRange: {
+          w: '',
+          N: '',
+          S: '',
+          E: ''        
         }
       },
       depthRangeConditions: [
@@ -155,7 +233,7 @@ export default {
         'Down(<)',
         'Equal(=)',
         'Between([a,b])',
-        'Unknow'
+        'Other'
       ]
     };
   },
@@ -165,7 +243,76 @@ export default {
   },
 
   methods: {
-    
+    searchConfirm() {
+      if(
+        this.searchContent.srastudyText === '' &&
+        this.searchContent.bioProjectText === '' &&
+        this.searchContent.projectID === '' &&
+        this.searchContent.centerName === '' &&
+        this.searchContent.submission === '' &&
+        this.searchContent.depthRangeCondition === '' &&
+        this.searchContent.depthValue === '' &&
+        this.searchContent.depthRange.low === '' &&
+        this.searchContent.depthRange.high === '' &&
+        this.searchContent.llRange.N === '' &&
+        this.searchContent.llRange.E === '' &&
+        this.searchContent.llRange.S === '' &&
+        this.searchContent.llRange.w === ''
+      ) {
+        this.$message({
+          message: 'Please input search conditions',
+          type: 'error'
+        });
+        return
+      } 
+
+      if(this.searchContent.depthRangeCondition === 'Between([a,b])') {
+        if(!(this.searchContent.depthRange.low || this.searchContent.depthRange.high)) {
+          this.$message({
+            message: 'Please input depth range',
+            type: 'error'
+          });
+          return
+        }
+      }
+      
+      if(
+        this.searchContent.depthRangeCondition === 'Up(>)' ||
+        this.searchContent.depthRangeCondition === 'Down(<)' ||
+        this.searchContent.depthRangeCondition === 'Equal(=)'
+      ) {
+        if(!this.searchContent.depthValue) {
+          this.$message({
+            message: 'Please input depth value',
+            type: 'error'
+          });
+          return
+        }
+      }
+      
+    },
+
+    searchConditionsReset() {
+      this.searchContent = {
+        srastudyText: '',
+        bioProjectText: '',
+        projectID: '',
+        centerName: '',
+        submission: '',
+        depthRangeCondition: '',
+        depthValue: '',
+        depthRange: {
+          low: '',
+          high: ''
+        },
+        llRange: {
+          w: '',
+          N: '',
+          S: '',
+          N: ''        
+        }
+      }
+    }
   },
 };
 </script>
@@ -212,6 +359,65 @@ export default {
         }
       }
     }
+
+    .longitude-and-latitude-range-container {
+      margin-top: 5px;
+      .up {
+        display: flex;
+        align-items: center;
+        .left {
+          display: flex;
+          align-items: center;
+          margin-top: 5px;
+          width: 50%;
+          padding-right: 5px;
+          .left-title {
+            width: 40px;
+          }
+        }
+        .right {
+          display: flex;
+          align-items: center;
+          margin-top: 5px;
+          width: 50%;
+          padding-left: 5px;
+          .right-title {
+            width: 40px;
+          }
+        }
+      }
+
+      .down {
+        display: flex;
+        align-items: center;
+        .left {
+          display: flex;
+          align-items: center;
+          margin-top: 5px;
+          width: 50%;
+          padding-right: 5px;
+          .left-title {
+            width: 40px;
+          }
+        }
+        .right {
+          display: flex;
+          align-items: center;
+          margin-top: 5px;
+          width: 50%;
+          padding-left: 5px;
+          .right-title {
+            width: 40px;
+          }
+        }
+      }
+    }
+  }
+
+  .button-container {
+    display: flex;
+    margin-top: 20px;
+    justify-content: center;
   }
 }
 </style>
--- src/views/database/overview/Overview.vue
+++ src/views/database/overview/Overview.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    Overview
+    <GeoVis></GeoVis>
   </div>
 </template>
 
 <script>
+import GeoVis from '@/components/visiualization/GeoVis.vue';
 export default {
   name: 'Overview',
+
+  components: {
+    GeoVis
+  },
 
   data() {
     return {
