d2eef5ea3c95cb6b38d46f15df5ec5f38dd8dacf
Tecot <tyx_cqbs@163.com>
2024-12-24 14:52:54 +0800

    20241224

--- vue.config.js
+++ vue.config.js
@@ -9,12 +9,6 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  chainWebpack: (conf) => {
-    conf.plugin('html').tap((args) => {
-      args[0].title = 'Marine Metagenome Database'
-      return args
-    })
-  },
   devServer: {
     proxy: {
       '/api': {
